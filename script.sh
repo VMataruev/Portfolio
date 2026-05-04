9d4cf5afe792dc1d943e15d710b5eea85837a87e
VMataruev <118466759+VMataruev@users.noreply.github.com>
2026-05-04 21:39:45 +0300

    .

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # === ПЕРЕМЕННЫЕ ===
-SRC_DIR="$HOME/images"
+SRC_DIR="/var/www/portfolio.net/imgs_1"
 IN_DIR="/var/www/portfolio.net/foto_in"
 OUT_DIR="/var/www/portfolio.net/foto_www"
 ARCH_DIR="/var/www/portfolio.net/arhiv/foto"
